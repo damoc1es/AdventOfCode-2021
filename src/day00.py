5dfe074518e4ba986729b595c6a705b782c69195
Damocles <38077049+damoc1es@users.noreply.github.com>
2022-11-28 19:10:40 +0200

    fix

--- src/day00.py
+++ src/day00.py
@@ -1,10 +1,12 @@
 # https://adventofcode.com/2021/day/0
 input = "data/input00.txt"
 
+
 def part1():
     with open(input, 'r') as file:
 
         return 0
+
 
 def part2():
     with open(input, 'r') as file:
