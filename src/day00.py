900bdfdc340e8ed9e661da8215a86c04d3333379
Damocles <38077049+damoc1es@users.noreply.github.com>
2022-11-28 19:09:46 +0200

    added template file to compare years

---
+++ src/day00.py
@@ -0,0 +1,16 @@
+# https://adventofcode.com/2021/day/0
+input = "data/input00.txt"
+
+def part1():
+    with open(input, 'r') as file:
+
+        return 0
+
+def part2():
+    with open(input, 'r') as file:
+
+        return 0
+
+
+print(f"Part 1: {part1()}")
+print(f"Part 2: {part2()}")
